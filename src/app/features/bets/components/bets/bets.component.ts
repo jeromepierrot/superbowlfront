077a911b7595eff586ed6be6e942236279828793
jpierrot <je.pierrot@gmail.com>
2023-07-19 15:57:19 +0200

    mod: betslip layout to match home layout + add CTA buttons

--- src/app/features/bets/components/bets/bets.component.ts
+++ src/app/features/bets/components/bets/bets.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { takeUntil } from 'rxjs/operators';
+import { FabPosition } from 'src/app/core/components/cta-fab/cta-fab.component';
+
 import { Bet } from 'src/app/core/models/bet';
 import { MatList, MatListItem } from '@angular/material/list';
 import { matchlist } from 'src/app/config/match-list.mock';
@@ -8,9 +14,16 @@ import { matchlist } from 'src/app/config/match-list.mock';
   templateUrl: './bets.component.html',
   styleUrls: ['./bets.component.css']
 })
-export class BetsComponent implements OnInit {
+export class BetsComponent implements OnInit, AfterViewInit, OnDestroy {
+  // global variables
+  @Input() isLogged = false;
+  fabPosition!: FabPosition;
+
+  // For MatDrawer variables
   isOpened: boolean = true;
   isEmpty: boolean = false;
+
+  // Mock variables
   matchListFiller = matchlist;
   betList: Bet[] = [
     {
@@ -47,8 +60,77 @@ export class BetsComponent implements OnInit {
     },
   ];
 
+    // Responsive Design
+    public breakpoint!: number;
+    public currentScreenSize!: string;
+    private destroyed = new Subject<void>();
+  
+    displayNameMap = new Map([
+      [Breakpoints.XSmall, 'XSmall'],
+      [Breakpoints.Small, 'Small'],
+      [Breakpoints.Medium, 'Medium'],
+      [Breakpoints.Large, 'Large'],
+      [Breakpoints.XLarge, 'XLarge'],
+    ]);
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router) {}
+
   ngOnInit() {
+    this.breakpoint = 6;
     this.isOpened = !this.isBetslipEmpty();
+  }
+
+  ngAfterViewInit(): void {
+    this.detectBreakpoint();
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed.next();
+    this.destroyed.complete();
+  }
+
+  detectBreakpoint() {
+    this.breakpointObserver
+    .observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge,
+    ]).pipe(takeUntil(this.destroyed))
+    .subscribe(result => {
+      for (const query of Object.keys(result.breakpoints)) {
+        if (result.breakpoints[query]) {
+          this.currentScreenSize = this.displayNameMap.get(query) ?? 'Unknown';
+        }
+        this.responsiveDisplay(this.currentScreenSize);
+      }
+    });
+  }
+
+  responsiveDisplay(currentScreenSize: string) {
+    switch(currentScreenSize) {
+      case 'XSmall':
+      case 'Small':
+        this.fabPosition = 'bottom';
+        this.breakpoint = 8;
+      break;
+      case 'Medium':
+        this.fabPosition = 'aside-bottom';
+        this.breakpoint = 6;
+      break; 
+      case 'Large':
+        this.fabPosition = 'aside';
+        this.breakpoint = 6;
+      break;
+      case 'XLarge':
+      default:
+        this.fabPosition = 'aside';
+        this.breakpoint = 5;
+      break;
+    }
   }
 
   isBetslipEmpty(): boolean {
